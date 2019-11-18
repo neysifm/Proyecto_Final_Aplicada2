8804756d5a384b7d45f31b91cc2f9bf056d970fd
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-11-17 22:57:29 -0400

    Add project files.

---
+++ Proyecto_Final_Aplicada2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Proyecto_Final_Aplicada2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
