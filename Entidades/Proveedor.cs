3488168d61907dac656805442d6203bdf2ed788e
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-11-20 15:41:07 -0400

    Creando mi estructura de Proyecto

---
+++ Entidades/Proveedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    [Serializable]
+    public class Proveedor
+    {
+        [Key]
+        public int ProveedorID { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
